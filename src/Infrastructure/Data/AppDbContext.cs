b5109980df4e395241c0256b5081ec0721fe61ad
Siarhei Naumau <s.naumau@gmail.com>
2022-07-09 18:51:51 +0300

    feat: implement application context

---
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Darkstore>? Darkstores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Server=DESKTOP-C2PJVTF\DEV;Database=CRM;Trusted_Connection=True;MultipleActiveResultSets=true",
+                    options => options.EnableRetryOnFailure());
+            }
+        }
+    }
+}
