6dd58241025b5b70d41eec2db03704f6d99df8d9
Siarhei Naumau <s.naumau@gmail.com>
2022-07-09 16:56:13 +0300

    feat: configure startup file

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseHttpsRedirection();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
