4c04385ca0808701a0028285952b3ab34a4a8723
Siarhei Naumau <s.naumau@gmail.com>
2022-07-09 18:57:54 +0300

    feat: implement application context factory

---
+++ src/Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(
+                @"Server=DESKTOP-C2PJVTF\DEV;Database=CRM;Trusted_Connection=True;MultipleActiveResultSets=true",
+                    options => options.EnableRetryOnFailure());
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
