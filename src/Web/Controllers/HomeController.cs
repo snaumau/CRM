a9246869b0b2f069824c75ffcbbe85d3667503f4
Siarhei Naumau <s.naumau@gmail.com>
2022-07-09 16:45:28 +0300

    feat: add home controller

---
+++ src/Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Welcom!\nThis page in development.";
+        }
+    }
+}
