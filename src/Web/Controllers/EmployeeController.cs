263d19a8149b42e7e5f82ecbc7da98a9f39b41a5
Siarhei Naumau <s.naumau@gmail.com>
2022-07-11 23:05:26 +0300

    feat: add employee controller

---
+++ src/Web/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IDarkstoreRepository _darkstoreRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository, IDarkstoreRepository darkstoreRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _darkstoreRepository = darkstoreRepository;
+        }
+
+        public ViewResult List(string darkstore)
+        {
+            IQueryable<Employee>? employees;
+            string? currentDarkstore;
+            if (string.IsNullOrEmpty(darkstore))
+            {
+                employees = _employeeRepository.Employees?
+                    .OrderBy(d => d.Id);
+                currentDarkstore = "AllDarkstores";
+            }
+            else
+            {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                employees = _employeeRepository.Employees?
+                    .Where(e => e.Darkstore.Name == darkstore)
+                    .OrderBy(e => e.Id);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                currentDarkstore = _darkstoreRepository.AllDarkstores?
+                    .FirstOrDefault(d => d.Name == darkstore)?.Name;
+            }
+
+            return View(new EmployeeListViewModel
+            {
+                Employees = employees,
+                CurrentDarkstore = currentDarkstore,
+            });
+        }
+    }
+}
