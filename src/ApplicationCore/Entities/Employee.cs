49bdaa0c90cc1a3de9e7ccb40178ae7c2a7b2def
Siarhei Naumau <s.naumau@gmail.com>
2022-07-11 19:14:08 +0300

    feat: add employee model

---
+++ src/ApplicationCore/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Position { get; set; }
+        public string? AccountNumber { get; set; }
+
+        public int DarkstoreId { get; set; }
+        public Darkstore? Darkstore { get; set; }
+    }
+}
