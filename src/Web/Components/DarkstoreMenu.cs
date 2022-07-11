29713c3ee1aed99220345591c19c55e761d13273
Siarhei Naumau <s.naumau@gmail.com>
2022-07-11 17:30:04 +0300

    feat: add component for dropdown menu

---
+++ src/Web/Components/DarkstoreMenu.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Components
+{
+    public class DarkstoreMenu : ViewComponent
+    {
+        private readonly IDarkstoreRepository? _darkstoreRepository;
+
+        public DarkstoreMenu(IDarkstoreRepository? darkstoreRepository)
+        {
+            _darkstoreRepository = darkstoreRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var darkstores = _darkstoreRepository?.AllDarkstores?
+                .OrderBy(d => d.DarkstoreId);
+            return View(darkstores);
+        }
+    }
+}
