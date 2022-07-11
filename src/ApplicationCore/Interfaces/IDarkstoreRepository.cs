ebcd291c3cfcddbc0939e78ac27abdd61ae0ca8d
Siarhei Naumau <s.naumau@gmail.com>
2022-07-11 17:16:02 +0300

    feat: add and implement interface for darkstore

---
+++ src/ApplicationCore/Interfaces/IDarkstoreRepository.cs
@@ -0,0 +1,9 @@
+﻿using ApplicationCore.Entities;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IDarkstoreRepository
+    {
+        IEnumerable<Darkstore>? AllDarkstores { get; }
+    }
+}
---
+++ src/Infrastructure/Repositories/DarkstoreRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories
+{
+    public class DarkstoreRepository : IDarkstoreRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DarkstoreRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Darkstore>? AllDarkstores => _appDbContext.Darkstores;
+    }
+}
