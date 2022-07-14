c06e165ec5d5c4ac29b7c99a59a0354b54038d08
Siarhei Naumau <s.naumau@gmail.com>
2022-07-14 20:52:22 +0300

    feat: update and add home view model to the controller

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IDarkstoreRepository _darkstoreRepository;
+
+        public HomeController(IDarkstoreRepository darkstoreRepository)
+        {
+            _darkstoreRepository = darkstoreRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                AllDarkstores = _darkstoreRepository.AllDarkstores,
+            };
+            return View(homeViewModel);
         }
     }
 }
