1dbb7eaffa56faecdecf4735c1631d81e16d60ae
Siarhei Naumau <s.naumau@gmail.com>
2022-07-16 20:39:30 +0300

    feat: add rate model

---
+++ src/ApplicationCore/Entities/Rate.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Rate
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
