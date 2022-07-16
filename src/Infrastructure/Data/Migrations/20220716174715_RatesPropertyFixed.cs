b67e8acae393b1861ecfe6520ef3a0c856fea49f
Siarhei Naumau <s.naumau@gmail.com>
2022-07-16 20:48:14 +0300

    fix: update database

---
+++ src/Infrastructure/Data/Migrations/20220716174715_RatesPropertyFixed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class RatesPropertyFixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Rates");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "RatePerHour",
+                table: "Rates",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RatePerHour",
+                table: "Rates");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Rates",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
